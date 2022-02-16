962fb6bcd3143a1288789dda8cef38418740fd9c
Zikani Nyirenda Mwase <zikaninyirenda@cgatechnologies.org.uk>
2022-02-16 18:12:50 +0200

    concept: Initiate Transfer Sessions from enrollment
    Added button and rudimentary functionality to initiate transfer sessions from enrollments. Have to figure out how to prevent duplicate sessions..

---
+++ sctp-core/src/main/java/org/cga/sctp/transfers/TransferSession.java
@@ -0,0 +1,76 @@
+/*
+ * BSD 3-Clause License
+ *
+ * Copyright (c) 2022, CGATechnologies
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.cga.sctp.transfers;
+
+import org.cga.sctp.persistence.DatabaseRecord;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="transfer_sessions_1")
+public class TransferSession extends DatabaseRecord {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private Long enrollmentSessionId;
+
+    @Column
+    private Boolean isClosed;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getEnrollmentSessionId() {
+        return enrollmentSessionId;
+    }
+
+    public void setEnrollmentSessionId(Long enrollmentSessionId) {
+        this.enrollmentSessionId = enrollmentSessionId;
+    }
+
+    public Boolean getClosed() {
+        return isClosed;
+    }
+
+    public void setClosed(Boolean closed) {
+        isClosed = closed;
+    }
+}
---
+++ sctp-mis/src/main/java/org/cga/sctp/mis/transfers/TransferSessionsController.java
@@ -0,0 +1,103 @@
+/*
+ * BSD 3-Clause License
+ *
+ * Copyright (c) 2022, CGATechnologies
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.cga.sctp.mis.transfers;
+
+import org.cga.sctp.mis.core.BaseController;
+import org.cga.sctp.targeting.CbtRanking;
+import org.cga.sctp.targeting.EnrollmentService;
+import org.cga.sctp.targeting.EnrollmentSessionView;
+import org.cga.sctp.transfers.TransferSession;
+import org.cga.sctp.transfers.TransferSessionService;
+import org.cga.sctp.user.RoleConstants;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Controller
+@RequestMapping("/transfers/sessions")
+public class TransferSessionsController extends BaseController  {
+
+    @Autowired
+    private TransferSessionService transferSessionService;
+
+    @Autowired
+    private EnrollmentService enrollmentService;
+
+    @PostMapping("/initiate")
+    @Secured({RoleConstants.ROLE_ADMINISTRATOR})
+    public String initiateNewTransferFromEnrollment(@RequestParam("enrollment") Long enrollmentSessionId,
+                                                    RedirectAttributes attributes) {
+        // 1. Select only the households that are marked as enrolled from the enrollment session
+        // 2. Create a new transfer_session (a session keeps track of transfer events), the transfer session may also track the periods
+        // 3. For each enrolled household create a new transfer_session_event (which calculates the amounts for each household)
+
+        EnrollmentSessionView sessionView = enrollmentService.getEnrollmentSession(enrollmentSessionId);
+        if (sessionView == null) {
+            return redirectWithDangerMessage(format("/targeting/enrollments?invalidSession=%s", enrollmentSessionId),
+                "Enrollment session was not found or not active",
+                attributes);
+        }
+        Long transferSessionId = -1L;
+        TransferSession session = new TransferSession();
+        session.setEnrollmentSessionId(enrollmentSessionId);
+        session.setClosed(false);
+        session.setActive(true);
+        session.setCreatedAt(LocalDateTime.now());
+        session.setModifiedAt(session.getCreatedAt());
+
+        transferSessionService.getTranferSessionRepository().save(session);
+
+        transferSessionId = session.getId();
+
+        Slice<CbtRanking> cbtRankingSlice = enrollmentService.getEnrolledHouseholds(sessionView, PageRequest.of(1,1000));
+        List<Long> householdIds  = cbtRankingSlice.getContent().stream().map(CbtRanking::getHouseholdId).collect(Collectors.toList());
+
+        transferSessionService.initiateTransfersForHouseholds(transferSessionId, householdIds, enrollmentSessionId);
+
+        return redirectWithSuccessMessage(format("/transfers/sessions/%s/assign-agency", transferSessionId),
+            "New Transfer Session initiated successfully from enrolled households",
+            attributes);
+    }
+}
