0361af5655d458fb4e173f751b0521c1219c95aa
Zikani Nyirenda Mwase <zikaninyirenda@cgatechnologies.org.uk>
2022-02-21 17:27:12 +0200

    chore: cleanup controller

--- sctp-mis/src/main/java/org/cga/sctp/mis/transfers/TransferParametersController.java
+++ sctp-mis/src/main/java/org/cga/sctp/mis/transfers/TransferParametersController.java
@@ -1,18 +1,14 @@
 package org.cga.sctp.mis.transfers;
 
 import org.cga.sctp.mis.core.BaseController;
-import org.cga.sctp.program.ProgramService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-@RestController
+@Controller
 @RequestMapping("/transfers/parameters")
 public class TransferParametersController extends BaseController {
-
-    @Autowired
-    private ProgramService programService;
-
     @GetMapping
     public ModelAndView getParametersList() {
         return view("transfers/parameters/index");
