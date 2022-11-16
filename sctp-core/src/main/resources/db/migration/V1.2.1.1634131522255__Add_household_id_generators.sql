3b554254533fd2903b7b8f7d4c953170136abcfa
sammymuriuki <samryuki@gmail.com>
2022-11-16 13:09:20 +0300

    fix id_counters table migration bug

--- sctp-core/src/main/resources/db/migration/V1.2.1.1634131522255__Add_household_id_generators.sql
+++ sctp-core/src/main/resources/db/migration/V1.2.1.1634131522255__Add_household_id_generators.sql
@@ -8,46 +8,46 @@ CREATE TABLE IF NOT EXISTS id_counters(
 	counter bigint NOT NULL,
 	CONSTRAINT UNIQUE unique_name (name)
 ) comment 'Keep track of various IDs. Values here must be seeded before dependent functions are called. ';
-
--- Initialize the counters
-INSERT INTO id_counters(name, counter)
-VALUES ('household_id', 300000);
-
--- the function has none of DETERMINISTIC, NO SQL, or READS SQL DATA in its declaration and binary logging is enabled
--- NOTE: run this SET GLOBAL log_bin_trust_function_creators = 1;
-
--- Create function to increment and return the ID counter
-DROP FUNCTION IF EXISTS getAndIncrementHouseholdId;
-
-DELIMITER $$
-CREATE FUNCTION getAndIncrementHouseholdId(_name varchar(50))
-RETURNS BIGINT
-comment 'Increments the household ID counter and returns the next ID to be assigned. If an error occurs between calling this procedure and the returned value being used, the value will be lost and it will create a gap in the sequence'
-BEGIN
-	DECLARE nextId bigint;
-	SET nextId = (SELECT counter FROM id_counters WHERE name = _name);
-	IF nextId IS NOT NULL THEN
-		UPDATE id_counters SET counter = counter + 1 WHERE name = _name;
-	END IF;
-	RETURN COALESCE(nextId, 0);
-END
-$$
-
-DELIMITER ;
-
 --
--- Create a trigger to assign household IDs
+---- Initialize the counters
+--INSERT INTO id_counters(name, counter)
+--VALUES ('household_id', 300000);
 --
-DROP TRIGGER IF EXISTS generateHouseholdMlCode;
-
-DELIMITER $$
-$$
-CREATE TRIGGER generateHouseholdMlCode
-BEFORE INSERT ON households
-FOR EACH ROW
-BEGIN
-	IF NEW.ml_code IS NULL THEN
-		SET NEW.ml_code = (SELECT getAndIncrementHouseholdId('household_id'));
-	END IF;
-END$$
-DELIMITER ;
+---- the function has none of DETERMINISTIC, NO SQL, or READS SQL DATA in its declaration and binary logging is enabled
+---- NOTE: run this SET GLOBAL log_bin_trust_function_creators = 1;
+--
+---- Create function to increment and return the ID counter
+--DROP FUNCTION IF EXISTS getAndIncrementHouseholdId;
+--
+--DELIMITER $$
+--CREATE FUNCTION getAndIncrementHouseholdId(_name varchar(50))
+--RETURNS BIGINT
+--comment 'Increments the household ID counter and returns the next ID to be assigned. If an error occurs between calling this procedure and the returned value being used, the value will be lost and it will create a gap in the sequence'
+--BEGIN
+--	DECLARE nextId bigint;
+--	SET nextId = (SELECT counter FROM id_counters WHERE name = _name);
+--	IF nextId IS NOT NULL THEN
+--		UPDATE id_counters SET counter = counter + 1 WHERE name = _name;
+--	END IF;
+--	RETURN COALESCE(nextId, 0);
+--END
+--$$
+--
+--DELIMITER ;
+--
+----
+---- Create a trigger to assign household IDs
+----
+--DROP TRIGGER IF EXISTS generateHouseholdMlCode;
+--
+--DELIMITER $$
+--$$
+--CREATE TRIGGER generateHouseholdMlCode
+--BEFORE INSERT ON households
+--FOR EACH ROW
+--BEGIN
+--	IF NEW.ml_code IS NULL THEN
+--		SET NEW.ml_code = (SELECT getAndIncrementHouseholdId('household_id'));
+--	END IF;
+--END$$
+--DELIMITER ;
