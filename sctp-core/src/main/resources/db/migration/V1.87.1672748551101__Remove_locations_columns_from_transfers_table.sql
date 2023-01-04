d72083684eecc6f3e44dfa007a3530e652f4227e
sammymuriuki <samryuki@gmail.com>
2023-01-04 14:24:04 +0300

    Remove redundant migration - V1.87.1672748551101

--- sctp-core/src/main/resources/db/migration/V1.87.1672748551101__Remove_locations_columns_from_transfers_table.sql
+++
@@ -1,5 +0,0 @@
--- Removes these redundant columns as they can be pulled from enrollment
-ALTER TABLE transfers DROP COLUMN `district_id`;
-ALTER TABLE transfers DROP COLUMN `village_cluster_id`;
-ALTER TABLE transfers DROP COLUMN `traditional_authority_id`;
-ALTER TABLE transfers DROP COLUMN `zone_id`;
